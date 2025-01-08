bab3619e7afbafdae2fbe5330f296d5903ec5b2b
CreamMelonSoda <valerioscipioni01@gmail.com>
2025-01-08 17:21:59 +0100

    Aggiunta Arma e Proiettile
    titolo self explanatory

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public Transform weapon;
+    public float offset;
+    public Transform shotPoint;
+    public GameObject projectile;
+    public float timeBetweenShots;
+    float nextShotTime;
     [SerializeField] private float movementSpeed = 5f;
    
     private Vector2 _movementDirection;
@@ -26,6 +31,18 @@ public class Player : MonoBehaviour
         if (PlayerCanDash())
         {
             HandleDash();
+        }
+        // Rotazione Arma
+        Vector3 displacement = weapon.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angle = Mathf.Atan2(displacement.y, displacement.x) * Mathf.Rad2Deg;
+        weapon.rotation = Quaternion.Euler(0f, 0f, angle + offset);
+
+        //Spara Peppino, SPARAAAAAA!!!!!
+        if (Input.GetMouseButtonDown(0)){
+            if (Time.time > nextShotTime){
+                nextShotTime= Time.time + timeBetweenShots;
+                Instantiate(projectile, shotPoint.position, shotPoint.rotation);
+            }
         }
     }
     //metodo da implementare
