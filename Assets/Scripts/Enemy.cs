11e4d071c6a957efd76aed6fd0f8ffe02033b06f
Labbo28 <labonia.riccardo@gmail.com>
2025-01-15 14:05:11 +0100

    aggiunto una sprite al nemico e un animator controller con 3 stati, la logica per gestire le animazioni sta nella classe EnemyAnimator, apportate alcune modifiche alla classe Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,21 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class Enemy : MonoBehaviour, IDamageable
 {
+    //eventi per gestire le diverse animazioni del nemico
+    public event EventHandler OnEnemyMoving;
+    public event EventHandler OnEnemyStopMoving;
+    public event EventHandler OnEnemyAttacking;
+    
     [SerializeField] private float speed = 3f;
     [SerializeField] private Transform player;
     [SerializeField] private float damage = 5f;
     [SerializeField] private float actionRadius = 0.5f;
     private SpriteRenderer spriteRenderer;
+    private bool isMoving;
+    private bool isAttacking;
     
 
     public float Health { get; private set; } = 100f;
@@ -22,16 +32,52 @@ public class Enemy : MonoBehaviour, IDamageable
     void Start()
     {
         player = FindObjectOfType<Player>().transform;
+      
+    }
+
+    private void HandleEnemyMovement()
+    {
+        if (isAlive && player != null)
+        {
+            LookAtPlayer();
+            isMoving = true;
+            transform.position = Vector2.MoveTowards(transform.position, 
+                player.position, speed * Time.deltaTime);
+            OnEnemyMoving?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            isMoving = false;
+            OnEnemyStopMoving?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     void Update()
     {
-        if (isAlive && player != null)
+       HandleEnemyMovement();
+    }
+
+   
+    private void LookAtPlayer()
+    {
+        if (player != null)
         {
-            transform.position = Vector2.MoveTowards(transform.position, 
-                player.position, speed * Time.deltaTime);
+            // Calcola la direzione verso il giocatore
+            Vector3 direction = player.position - transform.position;
+
+            // Calcola l'angolo in radianti e converti in gradi
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            float offset = 90f;
+            // Imposta la rotazione del nemico lungo l'asse Z
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - offset));
         }
     }
+
+
+
+    
+
+
 
     public void TakeDamage(float damage)
     {
@@ -53,6 +99,8 @@ public class Enemy : MonoBehaviour, IDamageable
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player in range");
+            isAttacking = true;
+            OnEnemyAttacking?.Invoke(this, EventArgs.Empty);
             playerInRange = true;
             InvokeRepeating(nameof(InflictDamageToPlayer), 0f, 1f);
             spriteRenderer.color = Color.red;
@@ -65,7 +113,6 @@ public class Enemy : MonoBehaviour, IDamageable
         {
             Debug.Log("Player out of range");
             playerInRange = false;
-            spriteRenderer.color = Color.blue;
             CancelInvoke(nameof(InflictDamageToPlayer));
         }
     }
---
+++ Assets/Scripts/EnemyAnimator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    private Animator _enemyAnimator;
+    [SerializeField] private Enemy enemy;
+
+    private void Awake()
+    {
+        if (TryGetComponent<Animator>(out Animator animator))
+        {
+            _enemyAnimator = animator;
+        }
+        else
+        {
+            Debug.LogWarning("Nessun Animator presente in EnemyPrefab");
+        }
+    }
+
+    private void Start()
+    {
+        enemy.OnEnemyMoving += EnemyOnOnEnemyMoving;
+        enemy.OnEnemyStopMoving += EnemyOnOnEnemyStopMoving;
+        enemy.OnEnemyAttacking += EnemyOnOnEnemyAttacking;
+    }
+
+    private void EnemyOnOnEnemyMoving(object sender, EventArgs e)
+    {
+        if (_enemyAnimator != null)
+        {
+            _enemyAnimator.SetBool("IsWalking", true);
+        }
+    }
+
+    private void EnemyOnOnEnemyStopMoving(object sender, EventArgs e)
+    {
+        if (_enemyAnimator != null)
+        {
+            _enemyAnimator.SetBool("IsWalking", false);
+        }
+    }
+
+    private void EnemyOnOnEnemyAttacking(object sender, EventArgs e)
+    {
+        if (_enemyAnimator != null)
+        {
+            _enemyAnimator.SetTrigger("Attack"); 
+        }
+    }
+}
