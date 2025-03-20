0a14cf8da5797525f0cbb61165586387fb0c5f48
Labbo28 <labonia.riccardo@gmail.com>
2025-03-20 21:07:47 +0100

    migliorata la classe Weapon per far ruotare l'arma attorno ad un pivot

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,7 +8,9 @@ public class Weapon : MonoBehaviour
 
     [SerializeField] Transform weaponPrefab;
     [SerializeField] Transform shotPoint;
-    private float offset = 90f;
+
+    [SerializeField] private Transform pivotPoint;
+    
 
      void Update()
     {
@@ -16,12 +18,36 @@ public class Weapon : MonoBehaviour
         HandleWeaponRotation();
     }
 
-    private void HandleWeaponRotation()
+   private void HandleWeaponRotation()
+{
+    Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    mouseWorldPosition.z = 0f; 
+    
+    Vector3 direction = mouseWorldPosition - pivotPoint.position;
+    
+    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    
+    weaponPrefab.rotation = Quaternion.Euler(0f, 0f, angle );
+    
+    
+    float weaponDistance = 0.6f; 
+    Vector3 weaponPosition = pivotPoint.position + (direction.normalized * weaponDistance);
+    weaponPrefab.position = weaponPosition;
+
+    SpriteRenderer weaponSprite = weaponPrefab.GetComponent<SpriteRenderer>();
+    if (weaponSprite != null)
     {
-        Vector3 displacement = weaponPrefab.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float angle = Mathf.Atan2(displacement.y, displacement.x) * Mathf.Rad2Deg;
-        weaponPrefab.rotation = Quaternion.Euler(0f, 0f, angle+offset);
+        if (mouseWorldPosition.x < pivotPoint.position.x)
+        {
+          
+            weaponSprite.flipY = true;
+        }
+        else
+        {
+            weaponSprite.flipY = false;
+        }
     }
+}
 
     private void HandleShooting()
     {
