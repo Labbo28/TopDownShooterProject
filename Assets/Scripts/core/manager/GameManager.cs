7f30cb6f089fff3d75a272f9e6a72adde6cee2eb
Labbo28 <labonia.riccardo@gmail.com>
2025-07-27 13:26:49 +0200

    organizzate (più o meno) le directory degli scripts

--- Assets/Scripts/core/manager/GameManager.cs
+++ Assets/Scripts/core/manager/GameManager.cs
@@ -58,7 +58,7 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         gameTime += Time.deltaTime;
-        if(Mathf.FloorToInt(gameTime) != Mathf.FloorToInt(previousGameTime))
+        if (Mathf.FloorToInt(gameTime) != Mathf.FloorToInt(previousGameTime))
         {
             formattedTime = GameTimeDisplay.FormatGameTime(gameTime);
             OnGameTimeChanged?.Invoke();
